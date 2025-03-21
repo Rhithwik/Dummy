6913a0e2435b3254cf7a233fb84f2b72e9a2e230
Rhithwik <rhithwik12@gmail.com>
2025-03-21 11:54:26 +0530

    Update File1.php

--- File1.php
+++ File1.php
@@ -1,36 +1,61 @@
-In Drupal 10, CKEditor 5 plugins are registered using a combination of module configuration, YAML files, and JavaScript. Here's how a plugin is typically registered:
+modules/custom/mymodule/mymodule.ckeditor5.yml
+
+myplugin:
+  id: myplugin
+  label: 'My Custom Plugin'
+  description: 'Adds a custom button to CKEditor 5.'
+  class: '\Drupal\mymodule\Plugin\CKEditor5Plugin\MyPlugin'
+  library: 'mymodule/myplugin'
+  dependencies: []
 
 
----
+modules/custom/mymodule/src/Plugin/CKEditor5Plugin/MyPlugin.php
 
-1. Define the CKEditor Plugin in a JavaScript File
+<?php
 
-Create a JavaScript file for your plugin inside your module, for example:
+namespace Drupal\mymodule\Plugin\CKEditor5Plugin;
 
-modules/custom/sce_ckeditor_customization/js/plugins/showmore.js
+use Drupal\ckeditor5\CKEditor5PluginBase;
 
-This JavaScript file should define and register the plugin using CKEditor 5's API:
+/**
+ * Defines the "MyPlugin" plugin.
+ *
+ * @CKEditor5Plugin(
+ *   id = "myplugin",
+ *   label = @Translation("My Custom Plugin"),
+ *   module = "mymodule"
+ * )
+ */
+class MyPlugin extends CKEditor5PluginBase {
+}
+
+
+
+
+modules/custom/mymodule/js/plugins/myplugin.js
 
 import { Plugin } from '@ckeditor/ckeditor5-core';
 import { ButtonView } from '@ckeditor/ckeditor5-ui';
 
-export default class ShowMorePlugin extends Plugin {
+export default class MyPlugin extends Plugin {
   init() {
     const editor = this.editor;
 
-    editor.ui.componentFactory.add('showMore', (locale) => {
+    // Register the button in the UI
+    editor.ui.componentFactory.add('myplugin', (locale) => {
       const button = new ButtonView(locale);
 
       button.set({
-        label: 'Insert ShowMore',
+        label: 'Insert Custom Text',
         withText: true,
         tooltip: true
       });
 
+      // Define what happens when the button is clicked
       button.on('execute', () => {
         editor.model.change((writer) => {
           const insertPosition = editor.model.document.selection.getFirstPosition();
-          writer.insertText('<showmore>', insertPosition);
+          writer.insertText('[custom-tag]', insertPosition);
         });
       });
 
@@ -40,108 +65,14 @@ export default class ShowMorePlugin extends Plugin {
 }
 
 
----
-
-2. Register the Plugin in ckeditor5.plugins.yml
-
-In your module, create a file:
-
-modules/custom/sce_ckeditor_customization/config/schema/ckeditor5.plugins.yml
-
-Define your plugin like this:
-
-sce_ckeditor_customization_showmore:
-  id: showmore
-  label: 'ShowMore Button'
-  description: 'Adds a button to insert a <showmore> tag.'
-  class: '\Drupal\sce_ckeditor_customization\Plugin\CKEditor5Plugin\ShowMore'
-  library: 'sce_ckeditor_customization/showmore'
-  dependencies: []
 
 
----
 
-3. Create the CKEditor Plugin Class in PHP
+modules/custom/mymodule/mymodule.libraries.yml
 
-Define a PHP class to register the plugin with Drupal:
-
-modules/custom/sce_ckeditor_customization/src/Plugin/CKEditor5Plugin/ShowMore.php
-
-<?php
-
-namespace Drupal\sce_ckeditor_customization\Plugin\CKEditor5Plugin;
-
-use Drupal\ckeditor5\CKEditor5PluginBase;
-
-/**
- * Defines the "ShowMore" plugin.
- *
- * @CKEditor5Plugin(
- *   id = "showmore",
- *   label = @Translation("ShowMore"),
- *   module = "sce_ckeditor_customization"
- * )
- */
-class ShowMore extends CKEditor5PluginBase {
-
-}
-
-
----
-
-4. Define a Drupal Library
-
-Create a libraries.yml file inside your module:
-
-modules/custom/sce_ckeditor_customization/sce_ckeditor_customization.libraries.yml
-
-showmore:
+myplugin:
   version: 1.x
   js:
-    js/plugins/showmore.js: { type: module }
+    js/plugins/myplugin.js: { type: module }
   dependencies:
     - core/ckeditor5
-
-
----
-
-5. Enable the Plugin in a Text Format
-
-1. Go to Configuration > Content Authoring > Text formats and editors (/admin/config/content/formats).
-
-
-2. Edit the text format where you want the plugin enabled (e.g., "Full HTML").
-
-
-3. Find your plugin (it should be listed as "ShowMore") and enable it.
-
-
-4. Save the changes.
-
-
-
-
----
-
-6. Clear Cache
-
-Run:
-
-drush cr
-
-Or go to Configuration > Performance and clear the cache.
-
-
----
-
-How It Works
-
-The JavaScript file defines the plugin behavior.
-
-The YAML and PHP files register the plugin with Drupal.
-
-The plugin is added to CKEditor when the text format is used.
-
-
-Now, when editing content, the "ShowMore" button should appear in the CKEditor toolbar.
-
